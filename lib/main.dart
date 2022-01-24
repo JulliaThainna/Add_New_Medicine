fb12c7e0a59f9b0d2a5847ddac2f065aafca38d7
JulliaThainna <julliathainna@outlook.com>
2022-01-24 17:40:23 -0300

    Finishing carousel

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
+import 'package:carousel_pro/carousel_pro.dart';
 import 'pallete.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -75,8 +74,8 @@ class MyCustomForm extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: const <Widget>[
-        Padding(
+      children: <Widget>[
+        const Padding(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 38),
           child: TextField(
             decoration: InputDecoration(
@@ -85,7 +84,7 @@ class MyCustomForm extends StatelessWidget {
                 borderSide: BorderSide.none,
               ),
               hintText: 'Enter the medicine name',
-              labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              labelStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
               floatingLabelBehavior: FloatingLabelBehavior.always,
               labelText: 'Medicine name\n\n',
               alignLabelWithHint: false,
@@ -94,7 +93,7 @@ class MyCustomForm extends StatelessWidget {
             ),
           ),
         ),
-        Padding(
+        const Padding(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
           child: TextField(
             textAlignVertical: TextAlignVertical.top,
@@ -106,24 +105,55 @@ class MyCustomForm extends StatelessWidget {
                 borderRadius: BorderRadius.all(Radius.circular(30.0)),
                 borderSide: BorderSide.none,
               ),
-              //contentPadding: EdgeInsetsDirectional.only(bottom: 100, start: 10, top: 20),
+              contentPadding:
+                  EdgeInsetsDirectional.only(bottom: 0, start: 10, top: 50),
               hintText: 'Enter the medicine description',
-              labelStyle: TextStyle(fontWeight: FontWeight.bold),
+              labelStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
               floatingLabelBehavior: FloatingLabelBehavior.always,
               labelText: 'Description\n\n',
               alignLabelWithHint: false,
               fillColor: Color(0xffe6e6e6),
               filled: true,
-              isDense: true, 
-              
+              isDense: true,
             ),
           ),
         ),
-
-
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            margin: const EdgeInsets.only(left: 30.0, right: 20.0),
+            child: Text(
+              'Medicine Type',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff646464),
+                  fontSize: 12),
+            ),
+          ),
+        ),
+        Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+            child: SizedBox(
+                height: 150.0,
+                width: double.maxFinite,
+                child: Carousel(
+                  images: const [
+                    NetworkImage(
+                        'https://www.statnews.com/wp-content/uploads/2021/10/molnu-capsule5.jpeg'),
+                    NetworkImage(
+                        'https://images.unsplash.com/photo-1573883429746-084be9b5cfca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGlsbHN8ZW58MHx8MHx8&w=1000&q=80'),
+                    ExactAssetImage("assets/images/LaunchImage.jpg")
+                  ],
+                  dotSize: 4.0,
+                  dotSpacing: 15.0,
+                  dotColor: Palette.blackToWhite.shade900,
+                  indicatorBgPadding: 5.0,
+                  dotBgColor: Palette.pinkToWhite,
+                  borderRadius: true,
+                  autoplay: false,
+                )))
       ],
     );
-    
   }
-  
 }
