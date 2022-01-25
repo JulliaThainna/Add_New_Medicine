aec6bf737d1d49d6fe8fabf05a452ab538634902
JulliaThainna <julliathainna@outlook.com>
2022-01-25 20:36:49 -0300

    Starting time shedule fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:carousel_pro/carousel_pro.dart';
+
 import 'pallete.dart';
 
 void main() {
@@ -9,7 +10,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,7 +37,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,20 +48,21 @@ class _MyHomePageState extends State<MyHomePage> {
         title: RichText(
           textAlign: TextAlign.left,
           text: const TextSpan(
-              text: 'Add New Medicine',
-              style: TextStyle(
-                  fontSize: 26,
-                  fontFamily: 'Montserrat',
-                  height: 1.5,
-                  color: Colors.white),
-              children: <TextSpan>[
-                TextSpan(
-                  text: '\nAnd set the infos',
-                  style: TextStyle(
-                    fontSize: 14,
-                  ),
+            text: 'Add New Medicine',
+            style: TextStyle(
+                fontSize: 26,
+                fontFamily: 'Montserrat',
+                height: 1.5,
+                color: Colors.white),
+            children: <TextSpan>[
+              TextSpan(
+                text: '\nAnd set the infos',
+                style: TextStyle(
+                  fontSize: 14,
                 ),
-              ]),
+              ),
+            ],
+          ),
         ),
       ),
       resizeToAvoidBottomInset: false,
@@ -69,8 +70,6 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
 
 class MyCustomForm extends StatefulWidget {
   const MyCustomForm({Key? key}) : super(key: key);
@@ -86,6 +85,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
+        //MEDICINE NAME FIELD
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 38),
           child: TextField(
@@ -96,7 +96,10 @@ class _MyCustomFormState extends State<MyCustomForm> {
               ),
               hintStyle: TextStyle(fontSize: 14),
               hintText: 'Enter the medicine name',
-              labelStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  color: Color(0xff666666)),
               floatingLabelBehavior: FloatingLabelBehavior.always,
               labelText: 'Medicine Name\n\n',
               alignLabelWithHint: false,
@@ -105,6 +108,8 @@ class _MyCustomFormState extends State<MyCustomForm> {
             ),
           ),
         ),
+
+        //MEDICINE DESCRIPTION FIELD
         const Padding(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
           child: TextField(
@@ -114,14 +119,17 @@ class _MyCustomFormState extends State<MyCustomForm> {
             maxLength: 300,
             decoration: InputDecoration(
               border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(30.0)),
+                borderRadius: BorderRadius.all(Radius.circular(30)),
                 borderSide: BorderSide.none,
               ),
               contentPadding:
                   EdgeInsetsDirectional.only(bottom: 0, start: 10, top: 50),
               hintStyle: TextStyle(fontSize: 14),
               hintText: 'Enter the medicine description',
-              labelStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  color: Color(0xff666666)),
               floatingLabelBehavior: FloatingLabelBehavior.always,
               labelText: 'Description\n\n',
               alignLabelWithHint: false,
@@ -131,15 +139,17 @@ class _MyCustomFormState extends State<MyCustomForm> {
             ),
           ),
         ),
+
+        //CAROUSEL
         Align(
           alignment: Alignment.centerLeft,
           child: Container(
-            margin: const EdgeInsets.only(left: 30.0, right: 20.0),
+            margin: const EdgeInsets.only(left: 30, right: 20.0),
             child: const Text(
               'Medicine Type',
               style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  color: Color(0xff646464),
+                  color: Color(0xff666666),
                   fontSize: 10.5),
             ),
           ),
@@ -147,7 +157,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
         Expanded(
           child: Container(
             alignment: Alignment.center,
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
             child: SizedBox(
               height: 130.0,
               width: double.maxFinite,
@@ -166,64 +176,198 @@ class _MyCustomFormState extends State<MyCustomForm> {
                 dotBgColor: Palette.pinkToWhite,
                 borderRadius: true,
                 autoplay: false,
+                //radius: const Radius.circular(30),
               ),
             ),
           ),
         ),
+
+        //QUANTITY FIELD + DROPDOWN BUTTON
         Expanded(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
             child: IntrinsicHeight(
               child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      child: Column(children: [
-                        TextField(
-                          textAlignVertical: TextAlignVertical.top,
-                          keyboardType: TextInputType.number,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly,
-                          ],
-                          maxLength: 4,
-                          decoration: const InputDecoration(
-                            counterText: "",
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(30.0)),
-                              borderSide: BorderSide.none,
-                            ),
-                            hintStyle: TextStyle(fontSize: 14),
-                            hintText: 'Enter the quantity',
-                            labelStyle: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 14),
-                            floatingLabelBehavior: FloatingLabelBehavior.always,
-                            labelText: 'Quantity\n\n',
-                            alignLabelWithHint: false,
-                            fillColor: Color(0xffe6e6e6),
-                            filled: true,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Column(children: [
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                        maxLength: 4,
+                        decoration: const InputDecoration(
+                          counterText: "",
+                          border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)),
+                            borderSide: BorderSide.none,
                           ),
+                          hintStyle: TextStyle(fontSize: 14),
+                          hintText: 'Enter the quantity',
+                          labelStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                              color: Color(0xff666666)),
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          contentPadding: EdgeInsetsDirectional.only(
+                              bottom: 10, start: 10, top: 0, end: 0),
+                          labelText: 'Quantity\n\n',
+                          alignLabelWithHint: false,
+                          fillColor: Color(0xffe6e6e6),
+                          filled: true,
                         ),
-                      ]),
-                    ),
-                    Expanded(
+                      ),
+                    ]),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsetsDirectional.only(
+                          bottom: 10, start: 10, top: 0, end: 10),
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                            color: const Color(0xffef6f86),
+                            borderRadius: BorderRadius.circular(30)),
                         child: DropdownButton(
-                            value: dropdownvalue,
-                            icon: const Icon(Icons.keyboard_arrow_down),
-                            items: items.map((String items){
-                                return DropdownMenuItem(
-                                  value: items,
-                                  child: Text(items),
-                                );
-                            }).toList(),
-                            onChanged: (String? newValue){
-                                setState(() {
-                                  dropdownvalue = newValue!;
-                                });
-                            },
-                          )
-                    )
-                  ]),
+                          style: const TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 14,
+                            color: Color(0xffffffff),
+                          ),
+                          underline: Container(
+                            height: 0,
+                          ),
+                          dropdownColor: Palette.pinkToWhite,
+                          borderRadius: BorderRadius.circular(30),
+                          isExpanded: true,
+                          value: dropdownvalue,
+                          icon: const Icon(Icons.keyboard_arrow_down),
+                          iconEnabledColor: const Color(0xffffffff),
+                          items: items.map((String items) {
+                            return DropdownMenuItem(
+                              value: items,
+                              child: Center(child: Text(items)),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownvalue = newValue!;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+
+        //TIME SHEDULE
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+            child: IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Column(children: [
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                        maxLength: 2,
+                        decoration: const InputDecoration(
+                          counterText: "",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                            borderSide: BorderSide.none,
+                          ),
+                          labelStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                            color: Color(0xff666666),
+                          ),
+                          floatingLabelBehavior: FloatingLabelBehavior.always,
+                          labelText: 'Frequency\n\n',
+                          alignLabelWithHint: false,
+                          fillColor: Color(0xffe6e6e6),
+                          filled: true,
+                        ),
+                      ),
+                    ]),
+                  ),
+                  Expanded(
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: const Color(0xffef6f86),
+                          borderRadius: BorderRadius.circular(30)),
+                      child: DropdownButton(
+                        style: const TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 14,
+                          color: Color(0xffffffff),
+                        ),
+                        underline: Container(
+                          height: 0,
+                        ),
+                        dropdownColor: Palette.pinkToWhite,
+                        isExpanded: true,
+                        value: dropdownvalue,
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        items: items.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Center(child: Text(items)),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownvalue = newValue!;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: const Color(0xffef6f86),
+                          borderRadius: BorderRadius.circular(30)),
+                      child: DropdownButton(
+                        style: const TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 14,
+                          color: Color(0xffffffff),
+                        ),
+                        underline: Container(
+                          height: 0,
+                        ),
+                        dropdownColor: Palette.pinkToWhite,
+                        isExpanded: true,
+                        value: dropdownvalue,
+                        icon: const Icon(Icons.keyboard_arrow_down),
+                        items: items.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Center(child: Text(items)),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            dropdownvalue = newValue!;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -231,4 +375,3 @@ class _MyCustomFormState extends State<MyCustomForm> {
     );
   }
 }
-
