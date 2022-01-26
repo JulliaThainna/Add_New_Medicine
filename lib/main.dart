69201875d3edcdeb0793654e0d236b66ba2bba24
JulliaThainna <julliathainna@outlook.com>
2022-01-26 01:38:56 -0300

    Finish frequency and time fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:carousel_pro/carousel_pro.dart';
-
+import 'package:intl/intl.dart';
 import 'pallete.dart';
 
 void main() {
@@ -79,8 +79,20 @@ class MyCustomForm extends StatefulWidget {
 }
 
 class _MyCustomFormState extends State<MyCustomForm> {
-  String dropdownvalue = 'Item 1';
-  var items = ['Item 1', 'Item 2', 'Item 3'];
+  TextEditingController timeinput = TextEditingController();
+
+  @override
+  void initState() {
+    timeinput.text = ""; //set the initial value of text field
+    super.initState();
+  }
+
+  String dropdownValueQ = 'Item 1';
+  var itemsQ = ['Item 1', 'Item 2', 'Item 3'];
+
+  String dropdownValueF = 'Item 1';
+  var itemsF = ['Item 1', 'Item 2', 'Item3'];
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -95,7 +107,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
                 borderSide: BorderSide.none,
               ),
               hintStyle: TextStyle(fontSize: 14),
-              hintText: 'Enter the medicine name',
+              hintText: 'Enter medicine name',
               labelStyle: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 14,
@@ -125,7 +137,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
               contentPadding:
                   EdgeInsetsDirectional.only(bottom: 0, start: 10, top: 50),
               hintStyle: TextStyle(fontSize: 14),
-              hintText: 'Enter the medicine description',
+              hintText: 'Enter medicine description',
               labelStyle: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 14,
@@ -206,7 +218,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
                             borderSide: BorderSide.none,
                           ),
                           hintStyle: TextStyle(fontSize: 14),
-                          hintText: 'Enter the quantity',
+                          hintText: 'Enter quantity',
                           labelStyle: TextStyle(
                               fontWeight: FontWeight.bold,
                               fontSize: 14,
@@ -225,7 +237,7 @@ class _MyCustomFormState extends State<MyCustomForm> {
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsetsDirectional.only(
-                          bottom: 10, start: 10, top: 0, end: 10),
+                          bottom: 10, start: 10, top: 0, end: 0),
                       child: DecoratedBox(
                         decoration: BoxDecoration(
                             color: const Color(0xffef6f86),
@@ -242,18 +254,18 @@ class _MyCustomFormState extends State<MyCustomForm> {
                           dropdownColor: Palette.pinkToWhite,
                           borderRadius: BorderRadius.circular(30),
                           isExpanded: true,
-                          value: dropdownvalue,
+                          value: dropdownValueQ,
                           icon: const Icon(Icons.keyboard_arrow_down),
                           iconEnabledColor: const Color(0xffffffff),
-                          items: items.map((String items) {
+                          items: itemsQ.map((String itemsQ) {
                             return DropdownMenuItem(
-                              value: items,
-                              child: Center(child: Text(items)),
+                              value: itemsQ,
+                              child: Center(child: Text(itemsQ)),
                             );
                           }).toList(),
                           onChanged: (String? newValue) {
                             setState(() {
-                              dropdownvalue = newValue!;
+                              dropdownValueQ = newValue!;
                             });
                           },
                         ),
@@ -266,10 +278,102 @@ class _MyCustomFormState extends State<MyCustomForm> {
           ),
         ),
 
-        //TIME SHEDULE
+        //FREQUENCY FIELD + DROPDOWN BUTTON
         Expanded(
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+            child: IntrinsicHeight(
+                child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Column(children: [
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
+                      maxLength: 2,
+                      decoration: const InputDecoration(
+                        counterText: "",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          borderSide: BorderSide.none,
+                        ),
+                        labelStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Color(0xff666666),
+                        ),
+                        floatingLabelBehavior: FloatingLabelBehavior.always,
+                        labelText: 'Frequency\n\n',
+                        hintStyle: TextStyle(fontSize: 14),
+                        hintText: 'Enter frequency',
+                        contentPadding: EdgeInsetsDirectional.only(
+                            bottom: 10, start: 10, top: 0, end: 0),
+                        alignLabelWithHint: false,
+                        fillColor: Color(0xffe6e6e6),
+                        filled: true,
+                      ),
+                    ),
+                  ]),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 5, right: 20),
+                    child: const Text(
+                      'times',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xff666666),
+                          fontSize: 14),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                        color: const Color(0xffef6f86),
+                        borderRadius: BorderRadius.circular(30)),
+                    child: DropdownButton(
+                      style: const TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontSize: 14,
+                        color: Color(0xffffffff),
+                      ),
+                      underline: Container(
+                        height: 0,
+                      ),
+                      dropdownColor: Palette.pinkToWhite,
+                      isExpanded: true,
+                      value: dropdownValueF,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      iconEnabledColor: const Color(0xffffffff),
+                      borderRadius: BorderRadius.circular(30),
+                      items: itemsF.map((String itemsF) {
+                        return DropdownMenuItem(
+                          value: itemsF,
+                          child: Center(child: Text(itemsF)),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValueF = newValue!;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            )),
+          ),
+        ),
+
+        //TIME AND DATA FIELDS
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
             child: IntrinsicHeight(
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -277,16 +381,15 @@ class _MyCustomFormState extends State<MyCustomForm> {
                   Expanded(
                     child: Column(children: [
                       TextField(
-                        keyboardType: TextInputType.number,
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly,
-                        ],
-                        maxLength: 2,
+                        controller:
+                            timeinput, //editing controller of this TextField
                         decoration: const InputDecoration(
+                          //icon of text field
+                          labelText: "Time\n\n",
                           counterText: "",
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.all(Radius.circular(30)),
-                            borderSide: BorderSide.none,
+                            borderSide: BorderSide.none, //label text of field
                           ),
                           labelStyle: TextStyle(
                             fontWeight: FontWeight.bold,
@@ -294,75 +397,69 @@ class _MyCustomFormState extends State<MyCustomForm> {
                             color: Color(0xff666666),
                           ),
                           floatingLabelBehavior: FloatingLabelBehavior.always,
-                          labelText: 'Frequency\n\n',
+                          hintStyle: TextStyle(fontSize: 14),
+                          hintText: 'Enter time',
+                          contentPadding: EdgeInsetsDirectional.only(
+                              bottom: 10, start: 10, top: 0, end: 0),
                           alignLabelWithHint: false,
                           fillColor: Color(0xffe6e6e6),
                           filled: true,
                         ),
-                      ),
+                        readOnly:
+                            true, //set it true, so that user will not able to edit text
+                        onTap: () async {
+                          TimeOfDay? pickedTime = await showTimePicker(
+                            initialTime: TimeOfDay.now(),
+                            context: context,
+                          );
+                          if (pickedTime != null) {
+                            DateTime parsedTime = DateFormat.jm()
+                                .parse(pickedTime.format(context).toString());
+                            String formattedTime =
+                                DateFormat('HH:mm:ss').format(parsedTime);
+                            setState(() {
+                              timeinput.text = formattedTime;
+                            });
+                          }
+                        },
+                      )
                     ]),
                   ),
                   Expanded(
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                          color: const Color(0xffef6f86),
-                          borderRadius: BorderRadius.circular(30)),
-                      child: DropdownButton(
-                        style: const TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 14,
-                          color: Color(0xffffffff),
+                    child: Padding(
+                      padding: const EdgeInsetsDirectional.only(
+                          bottom: 10, start: 10, top: 0, end: 0),
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                            color: const Color(0xffef6f86),
+                            borderRadius: BorderRadius.circular(30)),
+                        child: DropdownButton(
+                          style: const TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontSize: 14,
+                            color: Color(0xffffffff),
+                          ),
+                          underline: Container(
+                            height: 0,
+                          ),
+                          dropdownColor: Palette.pinkToWhite,
+                          borderRadius: BorderRadius.circular(30),
+                          isExpanded: true,
+                          value: dropdownValueQ,
+                          icon: const Icon(Icons.keyboard_arrow_down),
+                          iconEnabledColor: const Color(0xffffffff),
+                          items: itemsQ.map((String itemsQ) {
+                            return DropdownMenuItem(
+                              value: itemsQ,
+                              child: Center(child: Text(itemsQ)),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownValueQ = newValue!;
+                            });
+                          },
                         ),
-                        underline: Container(
-                          height: 0,
-                        ),
-                        dropdownColor: Palette.pinkToWhite,
-                        isExpanded: true,
-                        value: dropdownvalue,
-                        icon: const Icon(Icons.keyboard_arrow_down),
-                        items: items.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Center(child: Text(items)),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownvalue = newValue!;
-                          });
-                        },
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                          color: const Color(0xffef6f86),
-                          borderRadius: BorderRadius.circular(30)),
-                      child: DropdownButton(
-                        style: const TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontSize: 14,
-                          color: Color(0xffffffff),
-                        ),
-                        underline: Container(
-                          height: 0,
-                        ),
-                        dropdownColor: Palette.pinkToWhite,
-                        isExpanded: true,
-                        value: dropdownvalue,
-                        icon: const Icon(Icons.keyboard_arrow_down),
-                        items: items.map((String items) {
-                          return DropdownMenuItem(
-                            value: items,
-                            child: Center(child: Text(items)),
-                          );
-                        }).toList(),
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownvalue = newValue!;
-                          });
-                        },
                       ),
                     ),
                   ),
@@ -371,6 +468,23 @@ class _MyCustomFormState extends State<MyCustomForm> {
             ),
           ),
         ),
+
+        //BUTTON ADD MEDICINE
+        Padding(
+            padding: const EdgeInsetsDirectional.only(
+                start: 10, top: 0, end: 10, bottom: 10),
+            child: ElevatedButton(
+              child: const Text('Add New Medicine'),
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  primary: const Color(0xffef6f86),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  textStyle:
+                      const TextStyle(fontSize: 16, fontFamily: 'Montserrat')),
+              onPressed: () {},
+            )),
       ],
     );
   }
